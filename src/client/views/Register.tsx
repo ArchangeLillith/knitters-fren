698f5eb6adb497be5045d11358a2634518489a9e
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2024-08-09 07:20:45 -0400

    Added some scaffolding for when I come back to this

--- src/client/views/Register.tsx
+++ src/client/views/Register.tsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface RegisterProps {}
 
 const Register = (props: RegisterProps) => {
+	const [email, setEmail] = useState<string>("");
+	const [password, setPassword] = useState<string>("");
+	
 	return (
 		<div>
 			<h1>Register component rendered</h1>
+			<input
+				type="email"
+				value={email}
+				onChange={(e) => setEmail(e.target.value)}
+			/>
+			<input
+				type="password"
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+			/>
+			<p>Email: {email}</p>
+			<p>Password: {password}</p>
 		</div>
 	);
 };
