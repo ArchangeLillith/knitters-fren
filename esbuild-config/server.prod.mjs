91e58131d266400dfea14a122f645bf19164a53e
Aria Walford <90073620+ArchangeLillith@users.noreply.github.com>
2024-10-02 17:28:36 -0400

    Update server.prod.mjs

--- esbuild-config/server.prod.mjs
+++ esbuild-config/server.prod.mjs
@@ -1,21 +1,33 @@
 import * as esbuild from 'esbuild';
 
-try {
-	await esbuild.build({
-		entryPoints: ['src/client/index.tsx'],
-		bundle: true,
-		sourcemap: false,
-		minify: true,
-		outfile: 'public/static/bundle.js',
-		define: {
-			'process.env.NODE_ENV': "'production'",
-			'process.env.ROOT_URL':
-				"'https://arias-server-4642dbc777af.herokuapp.com'",
-		},
-	});
+(async () => {
+	try {
+		await esbuild.build({
+			entryPoints: ['src/server/server.ts'],
+			bundle: true,
+			sourcemap: true,
+			minify: true,
+			platform: 'node',
+			target: ['node20'],
+			outfile: 'dist/server.js',
+			external: [
+				'aws-sdk',
+				'mock-aws-s3',
+				'nock',
+				'@mapbox/node-pre-gyp',
+				'sqlite3',
+				'bcrypt',
+			],
+			define: {
+				'process.env.NODE_ENV': "'production'",
+				'process.env.ROOT_URL':
+					"'https://arias-server-4642dbc777af.herokuapp.com'",
+			},
+		});
 
-	console.log('Client bundled successfully for production!');
-} catch (error) {
-	console.error('An error occurred during bundling:', error);
-	process.exit(1);
-}
+		console.log('Server bundled successfully for production!');
+	} catch (error) {
+		console.error('An error occurred during bundling:', error);
+		process.exit(1); // Exit the process on build failure
+	}
+})();
