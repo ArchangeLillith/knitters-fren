d80765d24008f5e91b070e3e1a4aa45f8f58a6fc
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2024-10-02 16:30:29 -0400

    esbuild to deploy mode

--- esbuild-config/client.prod.mjs
+++ esbuild-config/client.prod.mjs
@@ -1,22 +1,22 @@
-import * as esbuild from "esbuild";
+import * as esbuild from 'esbuild';
 
 try {
 	await esbuild.build({
-		entryPoints: ["src/client/index.tsx"],
+		entryPoints: ['src/client/index.tsx'],
 		bundle: true,
 		sourcemap: false,
 		minify: true,
-		outfile: "public/static/bundle.js",
+		outfile: 'public/static/bundle.js',
 		define: {
-			"process.env.NODE_ENV": "'production'",
+			'process.env.NODE_ENV': "'production'",
 			//This is important! keyword: deploy
-			//Set this back to "https://arias-server-4642dbc777af.herokuapp.com" on delpoy
-			"process.env.ROOT_URL": "'http://localhost:3000'",
+			'process.env.ROOT_URL':
+				"'https://arias-server-4642dbc777af.herokuapp.com'",
 		},
 	});
 
-	console.log("Client bundled successfully for production!");
+	console.log('Client bundled successfully for production!');
 } catch (error) {
-	console.error("An error occurred during bundling:", error);
+	console.error('An error occurred during bundling:', error);
 	process.exit(1);
 }
